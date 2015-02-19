22d1390bbfc71f9c5fdb57162be96366c7d59ce2
frabbit <frabbit77@googlemail.com>
2015-02-19 01:24:06 +0100

    add sample for compile time types

---
+++ samples/04-types/Main.hx
@@ -0,0 +1,55 @@
+package ;
+
+import veins.di.Context;
+
+class Main {
+
+	static function main ()
+	{
+
+		var ctx = new Context()
+		.add(App.new)
+		.add( function ():Point return { x : 1, y : 1 } )
+		.add( function ():AppName return "MyApp" )
+		.add( function ():String return "someString" )
+		.add( Url.new.bind("http://myapp" ) )
+		.run(function (a:App) {
+			a.startup();
+		})
+		.bootstrap();
+	}
+}
+
+class App {
+
+	var p : Point;
+	var name : AppName;
+	var someString : String;
+	var url : Url;
+
+	public function new (p, name, url, str) {
+		this.p = p;
+		this.name = name;
+		this.url = url;
+		this.someString = str;
+	}
+
+	public function startup () {
+		trace(p);
+		trace(name);
+		trace(url);
+		trace(someString);
+	}
+}
+
+typedef AppName = String;
+
+typedef Point = { x : Int, y : Int };
+
+abstract Url(String) {
+
+	public function new (s:String) {
+		this = s;
+	}
+
+}
